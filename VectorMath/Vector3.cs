ce97899e9a378d00623d902f0e9142dcc5a44d57
ianwesleyarmstrong <ianwesleyarmstrong@F1092-40-2313>
2019-12-09 14:52:13 -0600

    finished lab

--- VectorMath/Vector3.cs
+++ VectorMath/Vector3.cs
@@ -18,11 +18,66 @@ namespace VectorMath
         }
 
         // Vector Addition
+        public static Vector3 Add(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+
+        public static Vector3 operator+(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+
+        public void Add(Vector3 other)
+        {
+            this.X += other.X;
+            this.Y += other.Y;
+            this.Z += other.Z;
+        }
 
         // Vector Subtraction 
+        // Vector Addition
+        public static Vector3 Subtract(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        }
+
+        public static Vector3 operator -(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        }
 
         // Vector Scaling 
+        public static Vector3 Scale(Vector3 a, double scalar)
+        {
+            return new Vector3(a.X * scalar, a.Y * scalar, a.Z * scalar);
+        }
+
+        // Vector Scaling 
+        public static Vector3 operator*(Vector3 a, double scalar)
+        {
+            return new Vector3(a.X * scalar, a.Y * scalar, a.Z * scalar);
+        }
 
         // Vector Comparison
+
+        public static bool operator ==(Vector3 a, Vector3 b)
+        {
+            return a.X == b.X && a.Y == b.Y && a.Z == b.Z;
+        }
+        public static bool operator !=(Vector3 a, Vector3 b)
+        {
+            return a.X != b.X || a.Y != b.Y || a.Z != b.Z;
+        }
+
+        public static readonly Vector3 Zero = new Vector3(0, 0, 0);
+
+        public static bool operator true()
+
+
+        public override string ToString()
+        {
+            return $"<{X}, {Y}, {Z}>";
+        }
     }
 }
